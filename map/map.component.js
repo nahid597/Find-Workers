5a3412fe3c8f3b5d2b31e9ede57d87edfc772afc
nahid597 <nahidh597@gmail.com>
2019-09-06 04:56:50 +0600

    map page updated.

--- map/map.component.js
+++ map/map.component.js
@@ -44,6 +44,10 @@ function initializeMap() {
         });
       });
     }
+    else
+    {
+      alert("please turn on your location service...");
+    }
 
     this.buildMap();
 
@@ -242,13 +246,16 @@ map.on('load', function () {
     
 }
 
+var popupCount = 0;
+
+
 function workerMarker(lng, lat,id, name, category, phone)
 {
 
      popup[id] = new mapboxgl.Popup({
         offset: 38,
-        closeOnClick: false,
-  
+        //closeOnClick: false,
+        
       })
        // .setHTML()
       .setHTML(generatedHtmlelemnets(id,name, category, phone));
