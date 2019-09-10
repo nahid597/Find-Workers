2c1ff3afaf6560aeda5c53bfd5ad875ef74f64f8
beta-ori <orion.beta.ori@gmail.com>
2019-09-10 22:58:17 +0600

    Established some socket connection.

--- backend/index.js
+++ backend/index.js
@@ -1,45 +1,57 @@
 /* This will run main server and connect to the database.*/
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const route = require('./route');
-const path = require("path");
-
-const app = express();
-const server = require('http').Server(app);
+const server = require("./server");
 const io = require('socket.io')(server);
 
-var db = 'mongodb+srv://khayrul1234:khayrul1234@cluster0-q6u9x.mongodb.net/test?retryWrites=true';
-var db0 = 'mongodb+srv://khayrul123:khayrul123@cluster0-6kqzz.mongodb.net/test?retryWrites=true';
-
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-
-
-mongoose.connect(db, { useNewUrlParser: true });
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true,
-}));
-
-app.use('', route);
-
-app.use(express.static(path.join(__dirname, '../')));
-
 var user = 0;
+var user_id = 10000000;
+
+var avail = {};
+var busy = {};
+var worker_socket = {};
+var fuser_socket = {};
+var cuser_socket = {};
+
 io.sockets.on('connection', function(socket) {
     user++;
+
+    socket.on('no_id', function(data) {
+        user_id++;
+        socket.emit('new_id', { id: user_id });
+        socket.id = user_id;
+        fuser_socket[user_id] = socket;
+    });
+
+    socket.on('connect_me', function(data) {
+        if (data.worker_id != null) {
+            socket.id = data.user_id;
+            cuser_socket[socket.id] = socket;
+        } else {
+            socket.id = data.user_id;
+            fuser_socket[socket.id] = socket;
+        }
+    });
+
+    socket.on('login', function(data) {
+        socket.id = data.worker_id;
+        worker_socket[socket.id] = socket;
+        data.user_id = null;
+        avail[data.worker_id] = data;
+    });
+
+    socket.on('confirmed', function(data) {
+        delete avail[data.worker_id];
+        busy[worker_id] = data;
+    });
+
+
+    socket.on('newLocation', function(data) {
+        if (cuser_socket[data.id] != undefined) {
+
+        }
+    })
     console.log(user + ' user connected');
     socket.on('disconnect', function() {
         user--;
         console.log(user + ' user connected');
     });
-});
-
-const port = 4444;
-server.listen(port, function() {
-    console.log('app listening on port ' + port);
 });
---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const route = require('./route');
+const path = require("path");
+
+const app = express();
+const server = require('http').Server(app);
+
+var db = 'mongodb+srv://khayrul1234:khayrul1234@cluster0-q6u9x.mongodb.net/test?retryWrites=true';
+var db0 = 'mongodb+srv://khayrul123:khayrul123@cluster0-6kqzz.mongodb.net/test?retryWrites=true';
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+
+mongoose.connect(db, { useNewUrlParser: true });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+
+app.use('', route);
+
+app.use(express.static(path.join(__dirname, '../')));
+
+const port = 4444;
+server.listen(port, function() {
+    console.log('app listening on port ' + port);
+});
+
+module.exports = server;
