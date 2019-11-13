a089b020bc9d99efb3ad4dec3f9ee1c45f36d619
Anichur25 <aniscseru6125@gmail.com>
2019-11-13 06:44:00 +0600

    updated

--- Front-End/src/app/app-routing.module.ts
+++ Front-End/src/app/app-routing.module.ts
@@ -43,6 +43,66 @@ const routes: Routes = [
   {
     path : 'help/customer',
     component : CustomerSectionComponent
+  },
+  {
+    path : 'help/worker',
+    component : WorkerSectionComponent
+  },
+  {
+    path : 'help/customer/How can I become a customer? What are the requirements?',
+    component : CustomerSectionComponent
+  },
+  {
+    path : 'help/customer/How to recover my block account?',
+    component : CustomerSectionComponent
+  },
+  {
+    path : 'help/customer/How to change account password?',
+    component : CustomerSectionComponent
+  },
+  {
+    path : 'help/customer/My app is not working,What should I do?',
+    component : CustomerSectionComponent
+  },
+  {
+    path : 'help/customer/How can I report an Issue/Complain?',
+    component : CustomerSectionComponent
+  },
+  {
+    path : 'help/customer/How I can get the Find-Worker app?',
+    component : CustomerSectionComponent
+  },
+  {
+    path : "help/customer/I can't sign into my account?",
+    component : CustomerSectionComponent
+  },
+  {
+    path : 'help/customer/I forget my password,What can I do?',
+    component : CustomerSectionComponent
+  },
+  {
+    path : "help/customer/I can't update my phone number or email.",
+    component : CustomerSectionComponent
+  },
+  {
+    path : "help/customer/My password reset link isn't working.",
+    component : CustomerSectionComponent
+  },
+  {
+    path : "help/customer/How to talk with a worker?",
+    component : CustomerSectionComponent
+  },
+  {
+    path : "help/customer/I want to update email,text or push notification settings.",
+    component : CustomerSectionComponent
+  },
+  {
+    path : "help/customer/What is Find-Worker system?",
+    component : CustomerSectionComponent
+  },
+  {
+    path : "help/customer/My worker’s number is unreachable. What should I do?",
+    component : CustomerSectionComponent
   }
 ];
 
