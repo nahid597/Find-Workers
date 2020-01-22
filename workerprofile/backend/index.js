3c276c48dbabeddb6f37f20a5780ecb36e8fd1d1
Anichur25 <aniscseru6125@gmail.com>
2020-01-22 08:53:49 +0600

    nothing to say

---
+++ workerprofile/backend/index.js
@@ -0,0 +1,75 @@
+const server = require("./server");
+const io = require('socket.io')(server);
+
+var user = 0;
+var user_id = 10000000;
+
+var avail = {};
+var busy = {};
+var worker_socket = {};
+var fuser_socket = {};
+var cuser_socket = {};
+
+io.sockets.on('connection', function(socket) {
+    user++;
+
+    socket.on('no_id', function(data) {
+        user_id++;
+        socket.emit('new_id', { id: user_id });
+        socket.id = user_id;
+        fuser_socket[user_id] = socket;
+    });
+
+    socket.on('connect_me', function(data) {
+        console.log(data);
+        if (data.worker_id != null) {
+            socket.id = data.user_id;
+            cuser_socket[socket.id] = socket;
+        } else {
+            socket.id = data.user_id;
+            fuser_socket[socket.id] = socket;
+        }
+    });
+
+    socket.on('worker_reconnect', function(data) {
+        socket.id = data.worker_id;
+        worker_socket[socket.id] = socket;
+    });
+
+    socket.on('login', function(data) {
+        socket.id = data.worker_id;
+        worker_socket[socket.id] = socket;
+        data.user_id = null;
+        avail[data.worker_id] = data;
+    });
+
+    socket.on('confirmed', function(data) {
+        delete avail[data.worker_id];
+        busy[worker_id] = data;
+    });
+
+
+    socket.on('newLocation', function(data) {
+        if (cuser_socket[data.user_id] != undefined) {
+            worker_socket[data.worker_id].emit('newLocation', data);
+        } else if (avail[data.worker_id] != undefined) {
+            fuser_socket.forEach(function(user_soc) {
+                user_soc.emit('newLocation', {});
+            });
+        } else if (busy[data.worker_id] != undefined) {
+            cuser_socket[data.user_id].emit('newLocation', data);
+        }
+    });
+
+    console.log(user + ' user connected');
+    socket.on('disconnect', function() {
+        if (worker_socket[socket.id] != undefined)
+            delete worker_socket[socket.id];
+        if (fuser_socket[socket.id] != undefined)
+            delete fuser_socket[socket.id];
+        if (cuser_socket[socket.id] != undefined)
+            delete cuser_socket[socket.id];
+        user--;
+        console.log(user + ' user connected');
+    });
+});
---
+++ workerprofile/workerprofile/workerprofile.component.js
@@ -0,0 +1,61 @@
+
+
+// defined arrow button function
+
+function arrowButton()
+{
+    location.href = "../map/map.component.html";
+}
+
+var _id = document.location.search.replace(/^.*?\=/, '');
+
+   console.log("worker id :" + _id);
+
+
+   var HttpClient = function()
+   {
+       this.get = function(aUrl, aCallback)
+       {
+            var anHtmlRequest = new XMLHttpRequest();
+            anHtmlRequest.onreadystatechange = function()
+            {
+                if(anHtmlRequest.readyState == 4 && anHtmlRequest.status == 200)
+                {
+                    aCallback(anHtmlRequest.responseText);
+                }
+            }
+
+            anHtmlRequest.open("GET", aUrl, true);
+            anHtmlRequest.send(null);
+       }
+   };
+
+   var client = new HttpClient();
+
+   this.client.get("http://localhost:4487/admin/workers?_id="+_id, function(respone){
+       var dbElement = JSON.parse(respone);
+        console.log(dbElement);
+
+        // for fixed digits afer point
+        var rate = dbElement[0].Rating.rating;
+        var category = dbElement[0].Catagory;
+
+        var storeCategory = "";
+
+  if(category === '0')
+     storeCategory = "কারেন্টের মিস্ত্রি";
+  else if(category === '1')
+      storeCategory = "গাড়ির মিস্ত্রি";
+  else if(category === '2')
+      storeCategory = "গ্যাসের চুলার মিস্ত্রি";
+  else if(category === '3')
+      storeCategory = "পানির লাইনের মিস্ত্রি";
+
+
+       var name = document.getElementById("workerName").innerHTML =  dbElement[0].Name;
+       var phone = document.getElementById("phone").innerHTML = dbElement[0].Phone ;
+       var category = document.getElementById("category").innerHTML = storeCategory  ;
+       var successfulWork = document.getElementById("successfulWork").innerHTML =  dbElement[0].Rating.count;
+       var rating = document.getElementById("rating").innerHTML =  parseFloat(Math.round(rate * 100) / 100).toFixed(1);
+       var reviews = document.getElementById("reviews").innerHTML =  dbElement[0].Rating.count * 5;
+    });
