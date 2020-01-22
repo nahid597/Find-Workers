7ba53b057d6b898959f9ac7428bcc6353c38653d
Anichur25 <aniscseru6125@gmail.com>
2020-01-22 12:28:08 +0600

    footer removed from each component

--- backend/route.js
+++ backend/route.js
@@ -7,9 +7,5 @@ const category = require('./routers/category');
 router.use('/admin/workers', workers);
 router.use('/admin/users', users);
 router.use('/admin/category', category);
-router.use('/admin/password/update',workers);
-router.use('/worker/phone-number-update',workers);
-router.use('/user/forgot/password',users);
-
 
 module.exports = router;
