b97482fc6fb57a52b512a6de881080fa23860d7b
Rakib31 <30778508+Rakib31@users.noreply.github.com>
2020-01-30 03:21:41 +0600

    push somthing

---
+++ backend/operation/operation.js
@@ -0,0 +1,34 @@
+//Find data from a database
+function findInf(model, query, callback) {
+    console.log(query);
+    model.find(query)
+        .exec(function(err, data) {
+            console.log(data);
+            if (err){
+                console.log(err);
+                return callback(err);
+            }
+            return callback(data);
+        });
+}
+
+//Insert into a database
+function insertData(model, callback) {
+    model.save(function(err) {
+        console.log(err);
+        if (err)
+            return callback(err);
+        return callback(true);
+    });
+}
+
+//Delete data from database
+function deleteData(model, query, callback) {
+    model.remove(query, function(err) {
+        if (err)
+            return callback(err);
+        return callback(true);
+    });
+}
+
+module.exports = { findInf, insertData, deleteData };
---
+++ backend/routers/category.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const Category = require('../models/category.model');
+const operation = require('../operation/operation');
+
+
+router.post('/post', (req, res, next) => {
+    
+    const category = new Category({
+        image: req.body.image,
+        title: req.body.title,
+        description: req.body.description,
+    });
+    category.save()
+    .then(result => {
+        res.status(200).json({
+            message: 'category saved',
+            result: result
+        });
+    })
+    .catch(err => {
+        res.status(201).json({
+            error: err,
+        });
+    });
+ });
+
+
+ router.get('/get', function(req, res) {
+
+    Category.find(req.query)
+        .exec(function(err, data) {
+            console.log(data);
+            if (err){
+                res.status(400).send(err);
+            }
+            else res.status(200).send(data);
+        });
+});
+ 
+
+ module.exports = router;
