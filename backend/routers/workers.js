6671e4ce0b4a1cc8374045ebc86ce1044ebcd83c
Nahid Hasan <30774439+nahid597@users.noreply.github.com>
2020-02-21 02:27:48 +0600

    Update workers.js

--- backend/routers/workers.js
+++ backend/routers/workers.js
@@ -6,118 +6,122 @@ const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const multer = require('multer');
 const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
+    destination: function(req, file, cb){
         cb(null, './upload/');
     },
-    filename: function(req, file, cb) {
+    filename: function(req, file, cb){
         cb(null, new Date().toISOString() + file.originalname);
     }
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({storage: storage});
 
 router.get('', function(req, res) {
-    operation.findInf(Worker, req.query, function(data, err) {
-        if (err != true) {
+    operation.findInf(Worker, req.query, function(data,err) {
+        if (err != true){
             res.status(200).json(data);
-        } else {
+        }
+        else{
             res.status(500).json(err);
         }
     });
 });
 
 router.post('/get', function(req, res) {
-    console.log("in ", req.body);
-    Worker.findOne({ _id: req.body._id })
-        .then(user => {
-            if (user) {
-                ob = {
-                    'userId': user
-                }
-                res.status(200).send(ob);
-            } else {
-                res.status(500).send(err);
+    console.log("in " , req.body);
+    Worker.findOne({_id: req.body._id})
+    .then(user => {
+        if(user) {
+            ob = {
+                'userId': user
             }
-        })
-        .catch(err => {
-            return res.status(404).json({
-                message: 'user not found'
-            });
+            res.status(200).send(ob);
+        }
+        else{
+            res.status(500).send(err);
+        }
+    })
+    .catch(err => {
+        return res.status(404).json({
+            message: 'user not found'
         });
+    });
 });
 
 
 
-router.post('/login', (req, res, next) => {
+router.post('/login', (req, res , next) => {
 
     let fetchData;
-
-    Worker.findOne({ Phone: req.body.Phone })
-        .then(user => {
-            if (!user) {
-                return res.status(404).json({
-                    message: 'Auth failed!'
-                });
-            }
-
-            fetchData = user;
-
-            return bcryptjs.compare(req.body.Password, fetchData.Password);
-        })
-        .then(result => {
-            if (!result) {
-                return res.status(404).json({
-                    message: 'Auth failed!'
-                });
-            }
-
-            const token = jwt.sign({ Phone: req.body.Phone, userId: fetchData._id }, "secret_should_more_difficult", {
-                expiresIn: '1h'
-            });
-
-            obj = {
-                'token': token,
-                'expiresIn': 3600,
-                'userId': fetchData
-            }
-
-            res.status(200).send(obj);
-
-        })
-        .catch(err => {
+   
+    Worker.findOne({Phone: req.body.Phone})
+    .then(user => {
+        if(!user)
+        {
             return res.status(404).json({
                 message: 'Auth failed!'
             });
+        }
+
+        fetchData = user;
+
+       return bcryptjs.compare(req.body.Password, fetchData.Password);
+    })
+    .then(result => {
+        if(!result)
+        {
+            return res.status(404).json({
+                message: 'Auth failed!'
+            });
+        }
+
+       const token = jwt.sign({Phone:req.body.Phone, userId: fetchData._id}, "secret_should_more_difficult" , {
+            expiresIn: '1h'
         });
+
+        obj = {
+            'token': token,
+            'expiresIn': 3600,
+            'userId': fetchData
+        }
+
+        res.status(200).send(obj);
+
+    })
+    .catch(err => {
+        return res.status(404).json({
+            message: 'Auth failed!'
+        });
+    });
 
 });
 
-router.post('/signup', upload.single('Image'), (req, res, next) => {
+router.post('/signup',upload.single('Image'), (req, res, next) => {
     console.log(req.file);
     bcryptjs.hash(req.body.Password, 10)
-        .then(hash => {
-            const user = new Worker({
-                Name: req.body.Name,
-                Phone: req.body.Phone,
-                Password: hash,
-                Category: req.body.Category,
-                Image: req.body.Image
-            });
-            user.save()
-                .then(result => {
-                    res.status(200).json({
-                        message: 'user created',
-                        result: result
-                    });
-                })
-                .catch(err => {
-                    res.status(201).json({
-                        error: err,
-                    });
-                });
+    .then(hash => {
+       const user = new Worker({
+            Name: req.body.Name,
+            Phone: req.body.Phone,
+            Password: hash,
+            Category: req.body.Category,
+            Image: req.body.Image
         });
-
-});
+        user.save()
+        .then(result => {
+            res.status(200).json({
+                message: 'user created',
+                result: result
+            });
+        })
+        .catch(err => {
+            res.status(201).json({
+                error: err,
+            });
+        });
+    });
+    
+ });
 
 router.delete('', function(req, res) {
     operation.deleteData(Worker, req.query, function(err) {
