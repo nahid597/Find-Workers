ac64e1e24d31d6690e181bf196775a52a0074e35
Nahid Hasan <30774439+nahid597@users.noreply.github.com>
2020-02-21 02:25:14 +0600

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const route = require('./route');
+const path = require("path");
+const cors = require('cors');
+
+const app = express();
+const server = require('http').Server(app);
+
+var db = 'mongodb+srv://khayrul1234:khayrul1234@cluster0-q6u9x.mongodb.net/test?retryWrites=true';
+var db0 = 'mongodb://localhost:27017/mydb';
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+
+mongoose.connect(db0, { useNewUrlParser: true }, ()=>{
+    console.log('ok');
+});
+
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+
+app.use('', route);
+
+app.use(express.static(path.join(__dirname, '../')));
+
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname , '../FrontEnd/navbar/navbar.component.html'));
+});
+const port = 4444;
+server.listen(port, function() {
+    console.log('app listening on port ' + port);
+});
+
+module.exports = server;
